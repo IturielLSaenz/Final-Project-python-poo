59c6b59e959db4cd32e4e94d248e9b1a86856034
IturielLSaenz <ilsaenzacademic@gmail.com>
2025-04-29 10:32:22 -0600

    Update eventos.py
    Updating main files for eventos handling.

--- guiFuncs/eventos.py
+++ guiFuncs/eventos.py
@@ -1,6 +1,7 @@
 # implementación GUI
 from tkinter import *
 from tkinter import font
+from tkinter import ttk
 import tkinter as tk
 
 # --- objetos ---
@@ -37,7 +38,7 @@ def addEvent():
     window = Toplevel()
     #window.protocol("WM_DELETE_WINDOW", lambda: None) # Eliminar el boton predeterminado para cerrar.
     #window.resizable(False, False)
-    window.geometry("540x340")
+    window.geometry("740x540")
     window.title("GUI - Sistema Agenda - 554644")
     window.columnconfigure(0,weight=1)
     window.columnconfigure(1,weight=1)
@@ -45,4 +46,49 @@ def addEvent():
     window.columnconfigure(3,weight=1)
 
     # --- Display ----
-    text = Label(window, text="-- Sistema Agenda - GUI - 554644 --", anchor="center").grid(columnspan=2, column=0, row=0,sticky="ew",pady=2)
+    text = Label(window, text="-- Sistema Agenda - GUI - 554644 --", anchor="center").grid(columnspan=4, column=0, row=0,sticky="ew",pady=2)
+    text = Label(window,text=" Añade un evento nuevo:").grid(columnspan=4,column=0,row=1,sticky="ew")
+    # --- formulario para rellenar y obtener un evento nuevo en la lista de eventos ---
+    # --- Ingresar contacto ---
+    text = Label(window,text="Contacto: ").grid(columnspan=1,column=0,row=2,sticky="ew")
+    contactoEntry = Entry(window,width=10)
+    # contactoEntry.config(state="disabled")
+    contactoEntry.grid(columnspan=2,column=1,row=2,sticky="ew")
+    contactoBtn = Button(window,text="Buscar").grid(columnspan=1,column=3,row=2,sticky="ew")
+    # --- Ingresar asunto ---
+    text = Label(window,text="Asunto: ").grid(columnspan=1,column=0,row=3,sticky="ew")
+    asuntoEntry = Entry(window,width=10)
+    asuntoEntry.grid(columnspan=3,column=2,row=3,sticky="ew")
+    # --- Ingresar fecha ---
+    # fecha: [entry] [entry] [entry]
+    text = Label(window,text="Fecha: ").grid(columnspan=1,column=0,row=4,sticky="ew")
+    diaEntry = Entry(window,width=10)
+    diaEntry.grid(columnspan=1,column=1,row=4,sticky="ew")
+    mesEntry = Entry(window,width=10)
+    mesEntry.grid(columnspan=1,column=2,row=4,sticky="ew")
+    annEntry = Entry(window,width=10)
+    annEntry.grid(columnspan=1,column=3,row=4,sticky="ew")
+    # --- Ingresar horaInicio ---
+    # Hora: [entry] [entry] [entry]
+    text = Label(window,text="Hora Inicio: ").grid(columnspan=1,column=0,row=5,sticky="ew")
+    horaInEntry = Entry(window,width=10)
+    horaInEntry.grid(columnspan=1,column=1,row=5,sticky="ew")
+    minInEntry = Entry(window,width=10)
+    minInEntry.grid(columnspan=1,column=2,row=5,sticky="ew")
+    secInEntry = Entry(window,width=10)
+    secInEntry.grid(columnspan=1,column=3,row=5,sticky="ew")
+    # --- Ingresar horaFin ---
+    # Hora: [entry] [entry] [entry]
+    text = Label(window,text="Hora Inicio: ").grid(columnspan=1,column=0,row=6,sticky="ew")
+    horaFnEntry = Entry(window,width=10)
+    horaFnEntry.grid(columnspan=1,column=1,row=6,sticky="ew")
+    minFnEntry = Entry(window,width=10)
+    minFnEntry.grid(columnspan=1,column=2,row=6,sticky="ew")
+    secFnEntry = Entry(window,width=10)
+    secFnEntry.grid(columnspan=1,column=3,row=6,sticky="ew")
+    # --- Ingresar descripción del evento ---
+    text = Label(window,text="Descripción: ").grid(columnspan=1,column=0,row=7,sticky="ew")
+    descEntry = Entry(window)
+    descEntry.grid(columnspan=3,column=1,row=7,sticky="ew")
+    # --- botón para terminar ---
+    submit = Button(window,text="Enviar",pady=30).grid(columnspan=2,column=1,row=8,sticky="ew")
