5ccc0eabd5c29a4b59cae94bb1c18026ee5914d5
IturielLSaenz <ilsaenzacademic@gmail.com>
2025-04-30 09:40:47 -0600

    Update eventos.py
    Search contact and select to just submit and create an event.

--- guiFuncs/eventos.py
+++ guiFuncs/eventos.py
@@ -12,7 +12,11 @@ from Agenda import Agenda as A
 
 from guiFuncs.contacts import listContactos
 
+finalContact = [None] # lista de un solo elemento para guardar la selección
+
 def openEventos():
+    # ----- Funcion para abrir la ventana de eventos -----
+    # Es la ventana principal de este apartado
     window = Toplevel()
     window.protocol("WM_DELETE_WINDOW", lambda: None) # Eliminar el boton predeterminado para cerrar.
     window.resizable(False, False)
@@ -47,16 +51,45 @@ def addEvent():
     window.columnconfigure(2,weight=1)
     window.columnconfigure(3,weight=1)
 
-    # --- special funcs for the GUI to work ---
+    # --- special funcs ---
     def searchContact():
-        pass
+        new_window = Toplevel()
+        new_window.geometry("400x500")
+        new_window.title("Buscar contacto")
+        new_window.columnconfigure(0,weight=1)
+        new_window.columnconfigure(1,weight=1)
+
+        def onClick(event):
+            selected_indices = contact_container.curselection() # los indices seleccionados están en la lista
+            if selected_indices:
+                selected = contact_container.get(selected_indices[0])
+                finalContact[0] = selected
+                print(f"Elemento seleccionado: {selected}")
+        def storeSelected():
+            if finalContact[0] is not None:
+                contactoEntry.insert(END,str(finalContact[0]))
+            new_window.destroy()
+        text = Label(new_window,text="Lista de contactos:").grid(columnspan=2,column=0,row=0,sticky="ew")
+        contact_container = Listbox(new_window,height=24,width=15,bg='grey',activestyle="dotbox",font="Helvetica",fg='white')
+        for contact in listContactos:
+            contact_container.insert(END,contact.toString())
+        contact_container.grid(column=0, row=1, columnspan=2, sticky="nsew", padx=10, pady=10)
+        contact_container.bind("<<ListboxSelect>>",onClick)
+        select = Button(new_window,text="Select",command=storeSelected).grid(columnspan=2,column=0,row=2)
+        return finalContact
+    
+    # --- submit sirve para crear el evento y terminar el proceso de esta ventana ---
     def submit():
         # --- declarar variables para crear los respectivos objetos ---
         subject = asuntoEntry.get() # Asunto para el evento
-        day = diaEntry.get()
-        month = mesEntry.get()
-        year = annEntry.get()
-        # Reemplazar todas las variables, solo poner los .get() dentro de el constructor del objeto
+        desc = descEntry.get() # Descripcion del evento
+        person = finalContact[0] # Persona del evento
+        fecha = D(int(annEntry.get()),int(mesEntry.get()),int(diaEntry.get())) # Crea la fecha para el evento
+        horaInicio = T(int(horaInEntry.get()),int(minInEntry.get()),int(secInEntry.get())) # Crea la hora de inicio
+        horaFin = T(int(horaFnEntry.get()),int(minFnEntry.get()),int(secFnEntry.get())) # Crea la hora de fin
+        # --- creando el evento ---
+        evento = E(subject,desc,person,fecha,horaInicio,horaFin)
+        print(evento.toString())
         window.destroy()
 
     # --- Display ----
@@ -68,7 +101,7 @@ def addEvent():
     contactoEntry = Entry(window,width=10)
     # contactoEntry.config(state="disabled")
     contactoEntry.grid(columnspan=2,column=1,row=2,sticky="ew")
-    contactoBtn = Button(window,text="Buscar").grid(columnspan=1,column=3,row=2,sticky="ew")
+    contactoBtn = Button(window,text="Buscar",command=searchContact).grid(columnspan=1,column=3,row=2,sticky="ew")
     # --- Ingresar asunto ---
     text = Label(window,text="Asunto: ").grid(columnspan=1,column=0,row=3,sticky="ew")
     asuntoEntry = Entry(window,width=10)
